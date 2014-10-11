7fcf8490969f65386089ea31f0bcd77542d81953
Soheil Koushan <soheil.koushan@gmail.com>
2014-10-11 16:34:11 -0400

    Added Robolectric

---
+++ app/src/test/java/com/skoushan/nyt/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.skoushan.nyt;
+
+import android.app.Activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void testSomething() throws Exception {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        assertTrue(activity != null);
+    }
+
+}
